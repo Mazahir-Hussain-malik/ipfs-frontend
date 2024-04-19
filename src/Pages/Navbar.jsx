5de7d05d98703e127ef579f79cb1869b63188028
Mazahir-Hussain-malik <ak8378670@gmail.com>
2024-04-19 18:48:49 +0500

    buttons are updated

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = ({ toggleSidebar }) => {
       </div>
       <div className="text-white font-bold text-xl mx-10 md:mx-0">
         <div className="flex items-center">
-            <img src="/ipfs_logo.png" alt="logo" width={40}/>
+            <img src="/logo.png" alt="logo" width={40}/>
             <h2 className="mx-2">Data Vortex AI</h2>
         </div>
 
@@ -33,7 +33,7 @@ const Navbar = ({ toggleSidebar }) => {
             </button> 
         :
 
-        <button onClick={() => open()} className="text-white">
+        <button onClick={() => open()} className=" flex items-center space-x-2 text-white  bg-[#164e63] hover:scale-105   focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 transition-all ease-linear delay-150 border-2 border-[#059669]">
           Connect Wallet
         </button>
         }
