dd36589e565e60bbc438bd1eb434098c088392c5
Mazahir-Hussain-malik <ak8378670@gmail.com>
2024-04-19 18:56:14 +0500

    upadte gradient

--- src/Pages/Sidebar.jsx
+++ src/Pages/Sidebar.jsx
@@ -84,7 +84,7 @@ const Sidebar = ({ isOpen, toggleSidebar, setCurrentPage, setUploadElem, openUpl
             </a>
           </li>
           <li>
-            <button href="#" onClick={() => handleLinkClick('Storage')} className="flex items-center space-x-2 text-white  bg-[#164e63] hover:scale-105   focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 transition-all ease-linear delay-150 border-2 border-[#059669]">
+            <button className="flex items-center space-x-2 text-white  bg-[#164e63] hover:scale-105   focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 transition-all ease-linear delay-150 border-2 border-[#059669]">
               
               Ai optimization
             </button>
